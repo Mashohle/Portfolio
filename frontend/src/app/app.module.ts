8cfc8f9d90fd4c957c120613edf8dfc251bb9d29
Mashohle Mangena <54825715+Mashohle@users.noreply.github.com>
2021-06-12 20:43:20 +0200

    Frontend files

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WorkComponent } from './components/work/work.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResumeComponent } from './components/resume/resume.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WorkComponent,
+    AboutComponent,
+    NavBarComponent,
+    SideBarComponent,
+    ContactComponent,
+    ProfileComponent,
+    ResumeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

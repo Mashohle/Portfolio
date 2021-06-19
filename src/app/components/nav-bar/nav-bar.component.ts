ab3f42f28732df7a31bb25602123b3e36098d3ff
Mashohle Mangena <54825715+Mashohle@users.noreply.github.com>
2021-06-19 21:26:04 +0200

    active nav bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  tab : any = 'profile';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // active class for nav bar selected tab
+  onClick(check){
+    if(check==1){
+      this.tab = 'profile';
+    }
+    else if(check==2){
+      this.tab = 'about';
+    }
+    else if(check==3){
+      this.tab = 'work';
+    }
+    else if(check==4){
+      this.tab = 'resume';
+    }
+    else if(check==5){
+      this.tab = 'contact';
+    }
+    else{
+      this.tab = 'profile';
+    }    
+  
+}
 
 }
